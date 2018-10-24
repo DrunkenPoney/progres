1f59611419b57ccb54102be7d3d16ebcc40d8c05
DrunkenPoney_Qc <drunkenponey@gmail.com>
2018-10-24 19:41:12 -0400

    Created poms

---
+++ TP3/client/src/main/java/tp3/controllers/MainController.java
@@ -0,0 +1,16 @@
+package tp3.controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainController extends Application {
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ TP3/serveur/src/main/java/tp3/Main.java
@@ -0,0 +1,10 @@
+package tp3;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Press any key to continue");
+		new Scanner(System.in).nextLine();
+	}
+}
